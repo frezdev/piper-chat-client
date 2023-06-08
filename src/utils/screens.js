acc956a6727196c94ac7cca18a558591739d0ae1
carlosandf <carlos.andres.ff2912@gmail.com>
2023-06-07 23:06:33 -0500

    Defining screen names

---
+++ src/utils/screens.js
@@ -0,0 +1,43 @@
+const auth = {
+  authStartScreen: 'AuthStartScreen',
+  loginScreen: 'LoginScreem',
+  registesScreen: 'ReggisterScreen'
+}
+
+const global = {
+  userProfileScreeb: 'userProfileScreen',
+  chatScreen: 'ChatScremm',
+  cameraScreen: 'CameraScreen',
+  imageScreen: 'ImageScreem',
+  grouScreen: 'GroupScreen',
+  groupProfileScreen: 'GroupProfileScreen',
+  addUserGroupScreen: 'AddUserGroupScreen',
+  changeNameGroupScreen: 'ChangeNameGroupScreen'
+}
+
+const chats = {
+  root: 'ChatsRoot',
+  chatScreen: 'ChatsRoot',
+  createChatScreem: 'CreateChatScreem'
+}
+
+const groups = {
+  root: 'GroupRoot',
+  chatgroScreen: 'ChatsRoot',
+  createGroupScreem: 'CreateGroupScreem'
+}
+
+const settings = {
+  root: 'SettingsRoot',
+  settingScreen: 'SettingsRoot',
+  createSettingScreem: 'CreateSettingScreem',
+  updateInfoUser: 'DpdateInfoUser'
+}
+
+export const screens = {
+  auth,
+  chats,
+  global,
+  groups,
+  settings
+}
