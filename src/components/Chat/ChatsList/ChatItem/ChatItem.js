84117976f0656edba68f6b66128ced1d10bfdd13
carlosandf <carlos.andres.ff2912@gmail.com>
2023-06-24 19:17:59 -0500

    Updating structure

--- src/components/Chat/ChatsList/ChatItem/ChatItem.js
+++ src/components/Chat/ChatsList/ChatItem/ChatItem.js
@@ -6,7 +6,7 @@ import { isEmpty } from 'lodash'
 import { DateTime } from 'luxon'
 import { useAuth } from '../../../../hooks'
 import { ChatMessage } from '../../../../api/chatMessage'
-import { ENV } from '../../../../utils'
+import { ENV, formatDate } from '../../../../utils'
 import { Styles } from './ChatItem.styles'
 
 const messageController = new ChatMessage()
@@ -17,6 +17,7 @@ export function ChatItem (props) {
   const { member_one, member_two } = chat
   const { user, accessToken } = useAuth()
   const [lastMessage, setLastMessage] = useState(null)
+  const [totalUnreadMessages, setTotalUnreadMessages] = useState(1)
 
   const userChat = user.id !== member_one.id ? member_one : member_two
 
@@ -33,60 +34,53 @@ export function ChatItem (props) {
   }, [chat._id])
   const openChat = () => {
     console.log('Abrir chat ->', chat._id)
-    console.log('user chat ->', userChat)
   }
-
-  const totalUnreadMessages = 12
 
   return (
     <>
-      {(lastMessage !== null) &&
-        (
-          <TouchableOpacity style={styles.item} onPress={openChat}>
-            <View style={{ paddingVertical: 7 }}>
-              <Avatar
-                style={styles.avatar}
-                bg={'lightBlue.600'}
-                marginRight={4}
-                marginX={2}
-                size='lg'
-                source={{ uri: userChat?.avatar && `${ENV.IMAGES_URL}/${userChat.avatar}` }}
-                // source={{ uri: user.avatar && `${ENV.IMAGES_URL}/${user.avatar}` }}
-              >
-                {userChat?.email.substring(0, 2).toUpperCase()}
-              </Avatar>
-            </View>
-            <View style={styles.infoContent}>
-              <View style={styles.info}>
-                <Text style={styles.identity} numberOfLines={1}>
-                  {
-                    (userChat?.firstName || userChat?.lastName)
-                      ? `${userChat?.firstName} ${userChat?.lastName}`
-                      : userChat?.email
-                  }
-                </Text>
-                <Text style={[styles.message, totalUnreadMessages > 0 && { fontWeight: '500', color: '#fff' }]} numberOfLines={2}>
-                  {lastMessage?.message || ''}
+      <TouchableOpacity style={styles.item} onPress={openChat}>
+        <View style={{ paddingVertical: 7 }}>
+          <Avatar
+            style={styles.avatar}
+            bg={'lightBlue.600'}
+            marginRight={4}
+            marginX={2}
+            size='lg'
+            source={{ uri: userChat?.avatar && `${ENV.IMAGES_URL}/${userChat.avatar}` }}
+            // source={{ uri: user.avatar && `${ENV.IMAGES_URL}/${user.avatar}` }}
+          >
+            {userChat?.email.substring(0, 2).toUpperCase()}
+          </Avatar>
+        </View>
+        <View style={styles.infoContent}>
+          <View style={styles.info}>
+            <Text style={styles.identity} numberOfLines={1}>
+              {
+                (userChat?.firstName || userChat?.lastName)
+                  ? `${userChat?.firstName} ${userChat?.lastName}`
+                  : userChat?.email
+              }
+            </Text>
+            <Text style={[styles.message, totalUnreadMessages > 0 && styles.unread]} numberOfLines={2}>
+              {lastMessage?.message || ''}
+            </Text>
+          </View>
+          <View style={styles.details}>
+            {lastMessage !== null && (
+              <Text style={[styles.time, totalUnreadMessages === 0 && styles.noNewMessages]}>
+                {formatDate(lastMessage?.createdAt)}
+              </Text>
+            )}
+            {totalUnreadMessages > 0 && (
+              <View style={[styles.totalUnreadMessagesContainer]}>
+                <Text style={styles.totalUnreadMessages}>
+                  {totalUnreadMessages < 99 ? totalUnreadMessages : '99'}
                 </Text>
               </View>
-              <View style={styles.details}>
-                {lastMessage !== null && (
-                  <Text style={[styles.time, totalUnreadMessages === 0 && styles.noNewMessages]}>
-                    {DateTime.fromISO(new Date(lastMessage?.createdAt).toISOString()).toRelativeCalendar()}
-                  </Text>
-                )}
-                {totalUnreadMessages > 0 && (
-                  <View style={[styles.totalUnreadMessagesContainer]}>
-                    <Text style={styles.totalUnreadMessages}>
-                      {totalUnreadMessages < 99 ? totalUnreadMessages : '99'}
-                    </Text>
-                  </View>
-                )}
-              </View>
-            </View>
-          </TouchableOpacity>
-        )
-      }
+            )}
+          </View>
+        </View>
+      </TouchableOpacity>
     </>
   )
 }
