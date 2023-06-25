2ae632b77dad562aa9cd32672c3d129bc568316c
carlosandf <carlos.andres.ff2912@gmail.com>
2023-06-24 19:13:22 -0500

    Add format date

---
+++ src/utils/formatDate.js
@@ -0,0 +1,18 @@
+export function formatDate (timestamp) {
+  const now = new Date()
+  const date = new Date(timestamp)
+
+  const timeDiff = now.getTime() - date.getTime()
+  const dayDiff = Math.floor(timeDiff / (1000 * 3600 * 12))
+
+  if (dayDiff === 0) {
+    return date.toLocaleTimeString(['es-CO'], { hour: '2-digit', minute: '2-digit' }) // Hoy: hh:mm
+  } else if (dayDiff === 1) {
+    return 'Ayer'
+  } else if (dayDiff >= 2 && dayDiff <= 7) {
+    const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+    return daysOfWeek[date.getDay()]
+  } else {
+    return date.toLocaleDateString(['es-CO'], { year: '2-digit', month: '2-digit', day: '2-digit' })
+  }
+}
