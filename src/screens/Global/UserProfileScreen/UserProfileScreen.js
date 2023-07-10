c55c55f61f72b7af2f52a4b77bbde35088cd4f82
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-09 20:51:37 -0500

    Update and use UserInfo component

--- src/screens/Global/UserProfileScreen/UserProfileScreen.js
+++ src/screens/Global/UserProfileScreen/UserProfileScreen.js
@@ -1,28 +1,15 @@
-import { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { Avatar } from 'native-base'
+import { View } from 'react-native'
 import { useCurrentChat } from '../../../hooks'
-import { ENV } from '../../../utils'
 import { Styles } from './UserProfileScreen.styles'
+import { UserInfo } from '../../../components/Settings/UserInfo/UserInfo'
 
 export function UserProfileScreen () {
   const { currentChat } = useCurrentChat()
-  const [user, setUser] = useState(null)
   const styles = Styles()
-
-  useEffect(() => {
-    setUser(currentChat.userTwo)
-  }, [])
 
   return (
     <View style={styles.content}>
-      <Avatar
-        source={{ uri: user?.avatar && `${ENV.IMAGES_URL}/${user?.avatar}` }}
-        style={styles.avatar}
-        size='xl'
-      >
-        {user?.email?.substring(0, 2).toUpperCase()}
-      </Avatar>
+      <UserInfo user={currentChat.userTwo} />
     </View>
   )
 }
--- src/screens/Global/UserProfileScreen/UserProfileScreen.styles.js
+++ src/screens/Global/UserProfileScreen/UserProfileScreen.styles.js
@@ -1,10 +1,9 @@
 import { StyleSheet } from 'react-native'
-import { Variables } from '../../../styles/variables.styles'
 
 export function Styles () {
-  const variables = Variables()
-
   return StyleSheet.create({
-    content: {}
+    content: {
+      marginVertical: 20
+    }
   })
 }
