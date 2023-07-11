6598089f7def1d861ce1c1dccd0d59a61dce32ab
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-10 22:33:19 -0500

    Add new feature

--- src/screens/Chats/ChatScreen.js
+++ src/screens/Chats/ChatScreen.js
@@ -1,9 +1,10 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import { ChatMessage, Chat } from '../../api'
 import { useAuth } from '../../hooks'
 import { HeaderChat } from '../../components/Navigation/HeaderChat'
+import { LoadingScreen } from '../../components/Shared'
 
 const messageController = new ChatMessage()
 const chatController = new Chat()
@@ -11,6 +12,19 @@ const chatController = new Chat()
 export function ChatScreen () {
   const { params } = useRoute()
   const { accessToken } = useAuth()
+
+  const [messages, setMessages] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await messageController.getAllMessage(accessToken, params?.chatId)
+        setMessages(response)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [])
 
   useEffect(() => {
     (async () => {
@@ -28,12 +42,18 @@ export function ChatScreen () {
       await chatController.setActiveChat('')
     }
   }, [])
+
   return (
     <>
       <HeaderChat chatId={params?.chatId || null} />
-      <View>
-        <Text>ChatScreen</Text>
-      </View>
+      {!messages
+        ? <LoadingScreen />
+        : (
+            <View>
+              <Text>ChatScreen</Text>
+            </View>
+          )
+      }
     </>
   )
 }
