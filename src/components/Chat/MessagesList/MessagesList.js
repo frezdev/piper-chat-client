aca4f013d4fe40d7de8c7f506b504691b62dda30
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-12 00:01:49 -0500

    Add MessagesList component

--- src/components/Chat/MessagesList/MessagesList.js
+++ src/components/Chat/MessagesList/MessagesList.js
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView } from 'react-native'
+import { View } from 'native-base'
+import { MessageImageItem, MessageTextItem } from './'
+import { styles } from './MessagesList.styles'
 
-export function MessagesList () {
+export function MessagesList (props) {
+  const { messages } = props
+  console.log(messages.at(-3))
   return (
-    <View>
-      <Text>MessagesList</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        {messages.map((message) => {
+          if (message.type === 'TEXT') {
+            return <MessageTextItem key={message._id} message={message}/>
+          }
+          if (message.type === 'IMAGE') {
+            return <MessageImageItem key={message._id} message={message} />
+          }
+        })}
+      </View>
+    </ScrollView>
   )
 }
