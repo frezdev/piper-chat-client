17c1c183648df99d86e653580c7981e643dc60a1
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-11 23:59:51 -0500

    Add new variable

--- src/styles/variables.styles.js
+++ src/styles/variables.styles.js
@@ -12,7 +12,8 @@ export function Variables () {
     secundaryBackground: dark ? '#0e0e0e' : '#f4f4f4',
     appBackground: dark ? '#000' : '#fff',
     borderColor: dark ? '#343434' : '#d0d0d0',
-    inputBg: dark ? '#a5a5a53b' : '#a5a5a52b'
+    inputBg: dark ? '#a5a5a53b' : '#a5a5a52b',
+    messageBg: dark ? '#00629a' : '#00a2ff'
   }
   return variables
 }
