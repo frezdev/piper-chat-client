7b91009a8d643bd1a079424021dc95885100a8af
carlosandf <carlos.andres.ff2912@gmail.com>
2023-07-12 19:23:36 -0500

    Export chatBgImage

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,7 +1,9 @@
 import auth01 from './auth-01.png'
 import auth02 from './auth-02.png'
+import chatBgImage from './chat-bg-image.png'
 
 export const images = {
   auth01,
-  auth02
+  auth02,
+  chatBgImage
 }
